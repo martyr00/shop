3f167934d17a4e06c3d28f084c7a8a2dc7434c0f
martyr00 <leonyvb04@gmail.com>
2023-06-27 20:45:42 +0300

    refactor

--- product/views.py
+++ product/views.py
@@ -11,22 +11,17 @@ class GETListOfProducts(generics.ListAPIView):
 
     def get_queryset(self):
         """Response list of products"""
-        category_id_from_url = self.kwargs['category_id']
+        return self.get_sort_queryset(self.get_queryset_by_category(Product.objects.all()))
+
+    def get_queryset_by_category(self, queryset):
+        """Get list of products by category"""
+        return queryset.filter(category=self.kwargs['category_id'])
+
+    def get_sort_queryset(self, queryset):
+        """Get sort list of products"""
         dictionary_from_id = self.request.query_params
 
-        return get_sort_queryset(dictionary_from_id.get('sort_by'),
-                                 dictionary_from_id.get('sort_dict'),
-                                 get_queryset_by_category(Product.objects.all(), category_id_from_url))
+        sort_dict = '-' if dictionary_from_id.get('sort_by') == 'desc' else ''
+        sort_by = 'title' if dictionary_from_id.get('sort_dict') is None else dictionary_from_id.get('sort_dict')
 
-
-def get_queryset_by_category(queryset, category_id):
-    """Get list of products by category"""
-    return queryset.filter(category=category_id)
-
-
-def get_sort_queryset(sort_by_from_url, sort_dict_from_url, queryset):
-    """Get sort list of products"""
-    sort_dict = '-' if sort_dict_from_url == 'desc' else ''
-    sort_by = 'title' if sort_by_from_url is None else sort_by_from_url
-
-    return queryset.order_by(sort_dict + sort_by)
+        return queryset.order_by(sort_dict + sort_by)
