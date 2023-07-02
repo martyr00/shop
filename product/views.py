434f4da087af533ad4238ab189e7e4f25d8b15cc
martyr00 <leonyvb04@gmail.com>
2023-07-02 17:42:48 +0300

    start app user_auth

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from django.db import IntegrityError
-from django.http import Http404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.exceptions import NotFound
