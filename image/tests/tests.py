709b38c1108bc8af7dce5398c9842668e5f6cbe4
martyr00 <leonyvb04@gmail.com>
2023-07-25 04:55:36 +0300

    start new app

---
+++ image/tests/tests.py
@@ -0,0 +1,36 @@
+# import json
+#
+# from django.test import TransactionTestCase
+# from http import HTTPStatus as HttpStatusCode
+#
+# from image.models import Image
+#
+#
+# # Create your tests here.
+#
+#
+# class ImageModelViewPostMethodTestCase(TransactionTestCase):
+#    """
+#    Image collection test case implementation.
+#    """
+#
+#     reset_sequences = True
+#
+#     def setUp(self):
+#         """
+#         Set up data for tests.
+#         """
+#         self.test_image_1 = Image.objects.create(
+#             title='test_image_1',
+#             image='...'
+#         )
+#         self.test_image_2 = Image.objects.create(
+#             title='test_image_2',
+#             image='...'
+#         )
+#
+#     def test_images(self):
+#         test_images = Image.objects.all()
+#
+#         assert test_images[0] == self.test_image_1
+#         assert test_images[1] == self.test_image_2
